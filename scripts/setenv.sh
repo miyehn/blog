5dd6eff0c67853da133a14a51175aae19dd2c0ba
miyehn <870340705@qq.com>
2018-12-19 16:55:22 -0500

    setenv

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -10,15 +10,17 @@ filepath = os.environ['HOME'] + '/.bash_profile'
 
 file = open(filepath,'a+')
 
-with open ('../package.json') as f:
+with open ('package.json') as f:
     data = json.load(f)
 
 domain = os.path.dirname(data['homepage'])
+content_dir = data['content_dir']
+scripts_dir = data['scripts_dir']
 
 file.write('\n# -------- mrblog setup --------\n')
 file.write('MRBLOG_DOMAIN="'+domain+'"\n')
-file.write('MRBLOG_CONTENT="'+data['content_dir']+'"\n')
-file.write('MRBLOG_SCRIPTS="'+data['scripts_dir']+'"\n')
+file.write('MRBLOG_CONTENT="'+content_dir+'"\n')
+file.write('MRBLOG_SCRIPTS="'+scripts_dir+'"\n')
 file.write('export MRBLOG_DOMAIN\nexport MRBLOG_CONTENT\nexport MRBLOG_SCRIPTS\n')
 file.write('alias mrblog="$MRBLOG_SCRIPTS/mrblog.sh"\n')
 file.write('# -------- end of setup --------\n')
@@ -29,16 +31,16 @@ os.system('source ' + filepath)
 
 # create empty content folder in content repo
 
-os.system('mkdir '+os.environ['MRBLOG_CONTENT'])
-os.system('mkdir '+os.environ['MRBLOG_CONTENT']+'/mrblog-content')
-os.system('mkdir '+os.environ['MRBLOG_CONTENT']+'/mrblog-content/blogposts')
-os.system('mkdir '+os.environ['MRBLOG_CONTENT']+'/mrblog-content/drafts')
-os.system('mkdir '+os.environ['MRBLOG_CONTENT']+'/mrblog-content/pics')
-os.system('mkdir '+os.environ['MRBLOG_CONTENT']+'/mrblog-content/tracks')
+os.system('mkdir '+content_dir)
+os.system('mkdir '+content_dir+'/mrblog-content')
+os.system('mkdir '+content_dir+'/mrblog-content/blogposts')
+os.system('mkdir '+content_dir+'/mrblog-content/drafts')
+os.system('mkdir '+content_dir+'/mrblog-content/pics')
+os.system('mkdir '+content_dir+'/mrblog-content/tracks')
 
 # edit .gitignore for content repo
 
-filepath = os.environ['MRBLOG_CONTENT'] + '/../.gitignore'
+filepath = content_dir + '/../.gitignore'
 with open (filepath, 'a+') as gitignore:
     gitignore.write('mrblog-content/drafts\n')
     gitignore.write('mrblog-content/localSummary\n')
