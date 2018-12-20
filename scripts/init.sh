08837c7d13f6589c19055d797236833c398f1a10
miyehniar <870340705@qq.com>
2018-12-20 02:46:27 -0500

    revert some changes

--- scripts/init.sh
+++ scripts/init.sh
@@ -39,18 +39,13 @@ os.system('mkdir '+content_dir+'/drafts')
 os.system('mkdir '+content_dir+'/pics')
 os.system('mkdir '+content_dir+'/tracks')
 
-# edit .gitignore for both repos
+# edit .gitignore for content repo
 print('edited .gitignore..')
 
 filepath = content_dir + '/../.gitignore'
 with open (filepath, 'a+') as gitignore:
     gitignore.write('mrblog-content/drafts\n')
     gitignore.write('mrblog-content/localSummary\n')
-
-filepath = scripts_dir + '/../.gitignore'
-with open (filepath, 'a+') as gitignore:
-    gitignore.write('*\n')
-    gitignore.write('!build')
 
 # install packages
 # amplitudejs had to be installed this way 
